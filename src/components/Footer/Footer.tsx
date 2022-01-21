97fc83b3f2d23fced924587dea66afc2dbd6e9dc
Vladislav Troshin <vlad89896135637@mail.ru>
2022-01-21 17:19:36 +0300

    Create React app

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import FooterClasses from "./Footer.module.scss"
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/MainCatalog/MainCatalog.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MainCatalogClasses from "./MainCatalog.module.scss"
+
+const MainCatalog = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default MainCatalog;
