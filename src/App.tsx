1ffbf8f3292eb5456184aec784d0fe0d7d8b9ac1
TheHaxy <vlad89896135637@mail.ru>
2022-01-23 17:05:42 +0300

    Create main catalog and fix catalogReducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 import store from "./store/store"
 import { Provider } from 'react-redux'
 
-import './App.css';
+import './App.scss';
+import MainCatalog from "./components/MainCatalog/MainCatalog";
 
 
 function App() {
   return (
       <Provider store={store}>
     <div className="App">
+      <MainCatalog/>
     </div>
       </Provider>
   );
